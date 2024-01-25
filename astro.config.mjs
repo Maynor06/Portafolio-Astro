9a67e7d142bde8207696600c57bb2088a1cd96be
Maynor <dmaynor488@gmail.com>
2024-01-25 10:06:55 -0600

    Arreglando el site en config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Maynor06.github.io',
+  site: 'https://maynor06.github.io',
   base: '/Portafolio-Astro',
 });
