e684e6f57b04ab2b47f71ecf878c06b439b40a0f
Maynor <dmaynor488@gmail.com>
2024-01-24 20:22:11 -0600

    Arreglando la ruta de la base en el config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Maynor06.github.io',
-  base: '/Portafolio-Astro',
+  base: '/Portafolio-Astro/',
 });
